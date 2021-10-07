d80d3014991f50db02f17eb28bd6d4c5951c12a2
Robert Samuel <robert_samuel97@hotmail.com>
2021-10-07 14:16:57 -0600

    feature: modify the main files for the startup of the project

--- client/main.jsx
+++ client/main.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
-import { App } from '/imports/ui/App';
+import FinanceCompact from '/imports/ui/FinanceCompact';
 
 Meteor.startup(() => {
-  render(<App/>, document.getElementById('react-target'));
+  render(
+    <FinanceCompact/>,
+    document.getElementById('react-target')
+  );
 });
