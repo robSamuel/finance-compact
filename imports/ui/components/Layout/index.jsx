160c205aecc83a93fecf7d7a54feff40c9603e34
Robert Samuel <robert_samuel97@hotmail.com>
2021-10-06 22:45:49 -0600

    feature: create the Layout component

---
+++ imports/ui/components/Layout/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useStyles } from './styles';
+import Header from '../Header';
+
+const Layout = ({ children }) => {
+    const classes = useStyles();
+
+    return (
+        <div>
+            {/** TODO: Add the Drawer here */}
+            <div className={classes.container}>
+                <Header />
+                <main className={classes.content}>
+                    <div className={classes.childrenContainer}>
+                        { children }
+                    </div>
+                </main>
+            </div>
+        </div>
+    );
+};
+
+Layout.propTypes = {
+    children: PropTypes.node.isRequired
+};
+
+export default Layout;
