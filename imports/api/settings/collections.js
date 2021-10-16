4a6e145c38abe45d31addc0de730b0b213e9fa03
Robert Samuel <robert_samuel97@hotmail.com>
2021-10-16 10:49:19 -0600

    feature: add the AccountsClasification collection

--- imports/api/settings/collections.js
+++ imports/api/settings/collections.js
@@ -1,3 +1,4 @@
 import { Mongo } from 'meteor/mongo';
 
 export const AccountsCatalog = new Mongo.Collection('accountsCatalog');
+export const AccountsClasification = new Mongo.Collection('accountsClasification');
--- imports/api/settings/server/publications.js
+++ imports/api/settings/server/publications.js
@@ -1,5 +1,10 @@
 import { AccountsCatalog } from '/imports/api/settings/collections';
+import { AccountsClasification } from '/imports/api/settings/collections';
 
 Meteor.publish('accountsCatalog', function() {
   return AccountsCatalog.find();
-})
+});
+
+Meteor.publish('accountsClasification', function() {
+  return AccountsClasification.find();
+});
